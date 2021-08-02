9b3eaf67594401c35fea1037e2a41a509dc0fd44
Dem-Ivan <demidenko.vanya@list.ru>
2021-08-02 17:23:05 +0300

    First commit

---
+++ SampleForDictionary/SampleForDictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleForDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
