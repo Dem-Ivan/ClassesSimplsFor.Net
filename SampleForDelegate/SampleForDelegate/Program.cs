04efbc2f01235a14e7bfe6eef8e2a1aa63173c6b
Dem-Ivan <demidenko.vanya@list.ru>
2021-08-09 21:03:31 +0300

    Added sample

---
+++ SampleForDelegate/SampleForDelegate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SampleForDelegate
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var bankAccount = new BancAccaunt();
+			var messHandler = new BancAccaunt.AccountMessageHandler(SendMessage);
+			messHandler += SendRedMessage;
+			bankAccount.DeligateRegister(messHandler);
+			bankAccount.Add(100);
+
+			//_____________________________
+
+			var test = new Test();
+
+			for (int i = 0; i < 10; i++)
+			{
+				test.Devide(10, i);
+			}
+		}
+
+		public static void SendMessage(string message)
+		{			
+			Console.WriteLine(message);			
+		}
+		public static void SendRedMessage(string message)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine(message);
+			Console.ResetColor();
+		}
+	}
+
+}
---
+++ SampleForDelegate/SampleForDelegate/Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleForDelegate
+{
+	public class Test
+	{
+		public void Devide(int a, int b)
+		{
+			try
+			{
+				int c = a / b;
+				if (b >= 5)
+				{
+					throw new NumberOver5Exception("Число b превысило 5");
+				}
+			}
+			catch (NumberOver5Exception e)
+			{
+				Console.WriteLine(e);
+			}
+			catch (DivideByZeroException e)
+			{
+				Console.WriteLine(e);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+			}
+			finally
+			{
+				Console.WriteLine("Блок finally выполнен");
+			}
+			
+		}
+	}
+}
