b4007e5f6f9e9798b37e3f109e111f7608dec041
Dem-Ivan <demidenko.vanya@list.ru>
2021-08-12 19:57:19 +0300

    added sempels

--- SampleForFileStream/SampleForFileStream/Program.cs
+++ SampleForFileStream/SampleForFileStream/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace SampleForFileStream
 {
@@ -6,7 +7,31 @@ namespace SampleForFileStream
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            string path = Path.Combine("D:", "Курсы.Net", "SemplesFor.Net", "SampleForFileStream", "TestFiles");
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            if (!directoryInfo.Exists)
+            {
+                directoryInfo.Create();
+            }
+
+            Console.WriteLine("Введите текст");
+            var text = Console.ReadLine();
+
+            using (FileStream fileStream = new FileStream($"{path}\\test.txt", FileMode.Append))
+            {
+                byte[] array = System.Text.Encoding.Default.GetBytes(text);
+                fileStream.Write(array, 0, array.Length);
+            }
+
+            using (FileStream fileStream = new FileStream($"{path}\\test.txt",FileMode.Open))
+            {
+                byte[] array2 = new byte[fileStream.Length];
+                fileStream.Read(array2, 0, array2.Length);
+                string readText = System.Text.Encoding.Default.GetString(array2);
+                Console.WriteLine(readText);
+            }
         }
     }
 }
