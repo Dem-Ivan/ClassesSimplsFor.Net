0b79fd58326cca7953e339851cfeb4083c3c862c
Dem-Ivan <demidenko.vanya@list.ru>
2021-08-16 17:18:22 +0300

    First commit

---
+++ SampleForSerialization/SampleForSerialization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleForSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
