271b3d8384d52be1a8aa68546c19a97e6c49e744
Dem-Ivan <demidenko.vanya@list.ru>
2021-08-19 18:44:11 +0300

    Second commit

---
+++ SampleForReflection/SampleForReflection/Figure.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleForReflection
+{
+   public class Figure
+    {
+        public string Name { get; set; }
+        public int SideCount { get; set; }
+        public double SideLenght{ get; set; }
+
+        public void Display(string name)
+        {
+            Console.WriteLine(name);
+        }
+
+    }
+}
--- SampleForReflection/SampleForReflection/Program.cs
+++ SampleForReflection/SampleForReflection/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace SampleForReflection
 {
@@ -6,7 +8,38 @@ namespace SampleForReflection
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //1
+            Type myType = typeof(Figure);
+
+            var myPropertis = myType.GetProperties();
+            var myMethods = myType.GetMethods();
+           
+            //2
+
+            var triangle = new Figure() {Name = "Triangle", SideCount = 3, SideLenght =1};
+            var myType2 = triangle.GetType();
+
+            //3
+
+            var myType3 = Type.GetType("SampleForReflection.Figure", false, true);
+
+
+            //4
+
+            Display(triangle); 
+        }
+
+        private static void Display<T>(T obj)
+        {
+            var myType = obj.GetType();
+            var properties = myType.GetProperties();
+
+            foreach (var property in properties)
+            {
+                var s = property.GetValue(obj);
+            }        
         }
     }
+
 }
