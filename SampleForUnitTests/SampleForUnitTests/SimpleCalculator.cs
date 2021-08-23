8d95206e3953c5f0daf807bc1843c8b3ebafe48d
Dem-Ivan <demidenko.vanya@list.ru>
2021-08-23 18:46:44 +0300

    first

---
+++ SampleForUnitTests/SampleForUnitTests/SimpleCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleForUnitTests
+{
+    public class SimpleCalculator
+    {
+        public int GetSum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int GetSubstruct(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ SampleForUnitTests/SampleFotUnitTests.Tests/SimpleCalculatorTests.cs
@@ -0,0 +1,39 @@
+﻿using SampleForUnitTests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SampleFotUnitTests.Tests
+{
+    public class SimpleCalculatorTests
+    {
+        [Fact]
+        public void GetSum_2_Plus_5_Eq_7()
+        {
+            //Arrange
+            SimpleCalculator calculator = new SimpleCalculator();
+
+            //Act
+            var resulr = calculator.GetSum(2, 5);
+
+            //Assert
+            Assert.Equal( 7, resulr);
+        }
+
+        [Fact]
+
+        public void GetSubstruct_5_Min_2_Eq_3()
+        {
+
+            //Arrange
+            SimpleCalculator calculator = new SimpleCalculator();
+
+            //Act
+            var resulr = calculator.GetSubstruct(5, 2);
+
+            //Assert
+            Assert.Equal(3, resulr);
+        }
+    }
+}
